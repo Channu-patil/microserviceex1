4f5146e0b5320ece6ce4191acf36d26b5f90c93e
Channu-patil <60845197+Channu-patil@users.noreply.github.com>
2022-09-18 14:33:26 +0530

    Update CompanyProxy.java

--- EmploeeService/src/main/java/com/citius/feign/CompanyProxy.java
+++ EmploeeService/src/main/java/com/citius/feign/CompanyProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.citius.dto.Company;
 
-@FeignClient(name="http://imc5bcp9-bll.mumbai1.corp.citiustech.com:8001/", url = "http://localhost:8001/")
+@FeignClient(name="COMPANY-SERVICE", url = "http://localhost:8001/")
 public interface CompanyProxy {
 	@GetMapping("/getCompany/{id}")
 	public Company getCompanyById(@PathVariable Integer id) ;
